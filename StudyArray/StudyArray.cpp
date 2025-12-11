0d7122a8d9dbd9aba6ce011ec08ecc2aa819c894
XuanHung-2903 <xuanhung29032004@gmail.com>
2025-12-11 22:31:37 +0700

    Mang sap xep

--- StudyArray/StudyArray.cpp
+++ StudyArray/StudyArray.cpp
@@ -1,31 +1,75 @@
-// StudyArray.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// StudyArray.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // enter array with many character numeric -> new array from small -> big numeric
 // 1,23,5,6,7,100 -> 1,5,6,7,23,100
 
 #include <iostream>
-
-#include <iostream>
 using namespace std;
 
-int main() 
-{
-    int a[] = { 1,23,5,6,7,100 };
-    int n = 6;
-
-    for (int i = 0; i < n - 1; i++) 
-    {   int minIndex = i;
-        for (int j = i + 1; j < n; j++) 
-        {   if (a[j] < a[minIndex])
-            minIndex = j;}
-        int temp = a[i];
-        a[i] = a[minIndex];
-        a[minIndex] = temp;}
-
-    cout << "Mang sau khi sap xep: ";
-    for (int i = 0; i < n; i++)
-        cout << a[i] << " ";
+void swapInt(int& a, int& b) {
+	int temp = a;
+	a = b;
+	b = temp;
 }
 
+void sortAscending(int a[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (a[i] > a[j]) {
+				swapInt(a[i], a[j]);
+			}
+		}
+	}
+}
+
+// Hàm sắp xếp giảm dần
+void sortDescending(int a[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (a[i] < a[j]) {
+				swapInt(a[i], a[j]);
+			}
+		}
+	}
+}
+
+int main()
+{
+	int n;
+	
+		int a[100] = {};
+	cout << " so phan tu";
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "số thứ: " << i + 1 <<" ";
+		cin >> a[i];
+	}
+	
+	int ch;
+	cout << "1. tang dan";
+	cout << "2. giam dan";
+	cin >> ch;
+	if (ch == 1) {
+		sortAscending(a, n);
+	}
+	else if (ch == 2) {
+		sortDescending(a, n);
+	}
+	else{
+		cout << "nhap sai roi";
+		return 0;
+	}
+
+	cout << "\nDay so sau khi sap xep: ";
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << " ";
+	}
+
+
+	return 0;
+
+}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
