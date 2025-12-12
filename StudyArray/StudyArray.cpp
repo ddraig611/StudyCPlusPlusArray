ccb52a4f064e5db9128132c3df60ea7b498707ed
XuanHung-2903 <xuanhung29032004@gmail.com>
2025-12-12 22:30:00 +0700

    B2 B3

--- StudyArray/StudyArray.cpp
+++ StudyArray/StudyArray.cpp
@@ -1,7 +1,8 @@
 ﻿// StudyArray.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // enter array with many character numeric -> new array from small -> big numeric
 // 1,23,5,6,7,100 -> 1,5,6,7,23,100
-
+// b2: nhập 2 số in ra tổng tích hiệu thương
+// b3: nhập vào 1 dãy số in ra số lớn nhất hoặc bé nhất
 #include <iostream>
 using namespace std;
 
@@ -32,11 +33,13 @@ void sortDescending(int a[], int n) {
 	}
 }
 
+
+
 int main()
 {
-	int n;
-	
-		int a[100] = {};
+	/*int n;
+
+	int a[100] = {};
 	cout << " so phan tu";
 	cin >> n;
 
@@ -45,7 +48,7 @@ int main()
 		cout << "số thứ: " << i + 1 <<" ";
 		cin >> a[i];
 	}
-	
+
 	int ch;
 	cout << "1. tang dan";
 	cout << "2. giam dan";
@@ -64,12 +67,87 @@ int main()
 	cout << "\nDay so sau khi sap xep: ";
 	for (int i = 0; i < n; i++) {
 		cout << a[i] << " ";
+	}*/
+
+	//B2
+
+	/*	double a, b;
+		cout << "so thu nhat";
+		cin >> a;
+		cout << "Nhap so thu hai: ";
+		cin >> b;
+		int Choose;
+		cout << "1. cong";
+		cout << "2. hieu";
+		cout << "3. tich";
+		cout << "4. thuong";
+		cin >> Choose;
+		double c;
+		if (Choose == 1) {
+			c = a + b;
+		}
+		else if (Choose == 2) {
+			c = a - b;
+		}
+		else if (Choose == 3) {
+			c = a * b;
+		}
+		else if (Choose == 4) {
+			if (b != 0){
+				c = a / b;
+			}
+			else {
+				cout << "phep tinh sai";
+			}
+		}
+		cout << "ket qua la" << c << "";
+	*/
+	//B3
+
+	int n;
+	int a[100];
+	cout << " so phan tu";
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cout << "nhap a[" << i << "]: ";
+		cin >> a[i];
+	}
+
+	int maxval = a[0];
+	int minval = a[0];
+
+
+	int Choose;
+	cout << "1. so lon nhat: " ;
+	cout << "2. so be nhat: " ;
+	cin >> Choose;
+	if (Choose == 1) {
+		for (int i = 1; i < n; i++) {
+			if (a[i] > maxval) {
+				maxval = a[i];
+			}
+
+		}
+
+	}
+	if (Choose == 2) {
+		for (int i = 1; i < n; i++) {
+			if (a[i] < minval)
+			{
+				minval = a[i];
+			}
+
+		}
+
 	}
 
 
-	return 0;
 
+
+
+	return 0;
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
